c15f304191ed5b8a52f198faf68cf4356791f87a
72310078-collab <72310078@students.liu.edu.lb>
2025-12-31 15:57:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -27,14 +27,14 @@ function App() {
 
       
           <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Home />} />
+            
+            <Route path="/Pure-Essence" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/shop" element={<Shop />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/cart" element={<Cart />} />
-            
+            <Route path="/login" element={<Login />} />
           
 
           </Routes>
